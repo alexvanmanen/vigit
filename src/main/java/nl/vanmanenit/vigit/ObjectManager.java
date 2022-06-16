ad69bbfb4b6428402ea107563404d6d53b6dca54
Alex van Manen <alexvanmanen@hotmail.com>
2022-06-16 22:12:45 +0000

    Remove unused import

--- src/main/java/nl/vanmanenit/vigit/ObjectManager.java
+++ src/main/java/nl/vanmanenit/vigit/ObjectManager.java
@@ -2,7 +2,6 @@ package nl.vanmanenit.vigit;
 
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
